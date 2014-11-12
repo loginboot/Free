8a1315b715f6ed5e0f64bf9df5f462b9960c9f8a
loginboot <loginboot@qq.com>
2014-11-12 16:41:24 +0800

    修改一些

--- FreeCrawler/src/com/fc/frame/CrawlerFrame.java
+++ FreeCrawler/src/com/fc/frame/CrawlerFrame.java
@@ -1,12 +1,25 @@
 package com.fc.frame;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import com.fc.config.Config;
+
+/**
+ * 
+ * @author lyodssoft.com
+ *
+ * @creator xiesw
+ * @version 1.0.0
+ * @date 2014-11-11
+ * @description 网页爬虫主界面类 - 创建
+ *
+ */
 
 public class CrawlerFrame extends JFrame
 {
 
+	
 	/**
 	 *  CW ID
 	 */
@@ -14,6 +27,10 @@ public class CrawlerFrame extends JFrame
 	
 	//配置文件
 	private static Config cfg = Config.getInstance();
+	
+	private JLabel lab1;
+	private JLabel lab2;
+	private JLabel lab3;
 	
 	public CrawlerFrame()
 	{
